5b051496c8976915b3d25177df9225130065a5fa
rajstormo <rajstormo@gmail.com>
2023-07-09 12:58:51 +0530

    done few changes

--- logical_operators.sql
+++ logical_operators.sql
@@ -1,22 +1,26 @@
 
 USE studentdata;
 
-#CREATING TABLE
+# creating table
 CREATE TABLE employee(id INT, name VARCHAR(20), salary INT, job VARCHAR(20), state VARCHAR(20));
 
-#INSERTING VALUES INTO TABLE
+# inserting values 
 INSERT INTO employee(id,name,salary,job,state) VALUES (1,"rajkumar", 80000,	"web developer","gujarat"),(2,"stormo",70000,"gamer","gujarat");
 INSERT INTO employee VALUES(5,"unknown",65000,"manager","mumbai");
 
+# WHERE CLAUSE: used to filter records
 SELECT* FROM employee;
 SELECT * FROM employee WHERE salary > 70000;
 
+# AND & OR operators
 SELECT * FROM employee WHERE salary >= 70000 AND state = "gujarat";
 SELECT * FROM employee WHERE salary > 70000 OR state = "gujarat";
 
-# select those employees whose salary is b/w [65000, 70000] (both inclusive)
+# BETWEEN: select those employees whose salary is b/w [65000, 70000] (both inclusive)
 SELECT * FROM employee WHERE salary BETWEEN 65000 AND 70000;
 
+# in place of using multiple OR's,  use IN operator
+SELECT * FROM employee WHERE job = "web developer" OR job = "software engineer";
 SELECT * FROM employee WHERE job IN("web developer","software engineer");
 SELECT * FROM employee WHERE state NOT IN ("gujarat");
 	
@@ -35,5 +39,6 @@ SELECT * FROM employee WHERE name LIKE "%a_";
 # states having 7 characters
 SELECT * FROM employee WHERE state LIKE "_______";
 
+# states having 7 characters but is starting from 'g'
 SELECT * FROM employee WHERE state LIKE "g______";
 
